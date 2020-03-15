bf936bffe081655eaac3feffadbc977d5d4fd05a
Sylvester <sylwek.tarnowski@gmail.com>
2020-03-15 22:17:23 +0100

    add navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+const Navbar = () => {
+  return (
+    <AppBar position="sticky">
+      <Toolbar></Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
