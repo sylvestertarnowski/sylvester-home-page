6f238b11875d4e38923fd69d83d30fc7890fac7c
Sylvester <34459277+sylvestertarnowski@users.noreply.github.com>
2020-04-05 15:23:40 +0200

    add contact info links, add content to home page

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Contact.css';
+import { LinkedIn, Email, GitHub } from '@material-ui/icons';
 
 const Contact: React.FC<any> = () => {
   return (
     <div>
-      <h1>Contact Works!</h1>
+      <h1>Places you can find me:</h1>
+      <h2>
+        <a
+          href="https://www.linkedin.com/in/sylvestertarnowski/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <LinkedIn /> Connect with me on LinkedIn
+        </a>
+      </h2>
+      <h2>
+        <a href="mailto:sylwek.tarnowski@gmail.com">
+          <Email /> Send me an Email
+        </a>
+      </h2>
+      <h2>
+        <a
+          href="https://github.com/sylvestertarnowski"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub /> Check out my GitHub profile
+        </a>
+      </h2>
     </div>
   );
 };
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -3,8 +3,12 @@ import './Home.css';
 
 const Home: React.FC<any> = () => {
   return (
-    <div>
-      <h1>Home Works!</h1>
+    <div className="Home">
+      <div className="Home-content">
+        <h1>Hi, I'm Sylvester</h1>
+        <h2>Front-end developer</h2>
+        <h3>I specialise in React</h3>
+      </div>
     </div>
   );
 };
